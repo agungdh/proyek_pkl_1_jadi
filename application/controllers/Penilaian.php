be5cb55cf107e0a5929281f53ca4fa1ee6081471
agungdh <agungdh@live.com>
2018-04-15 13:18:41 +0700

    update excel

--- application/controllers/Penilaian.php
+++ application/controllers/Penilaian.php
@@ -96,6 +96,124 @@ class Penilaian extends CI_Controller {
 	}
 
 	function export_excel($id_penilaian) {
+		$data['penilaian_id'] = $this->m_penilaian->ambil_penilaian_id($id_penilaian);
+		$data['penilaian'] = $this->m_penilaian->ambil_penilaian($data['penilaian_id']->pengajuan_id);
+		$data['pengajuan'] = $this->m_penilaian->ambil_pengajuan_id($data['penilaian_id']->pengajuan_id);
+		$data['standar'] = $this->m_penilaian->ambil_standar($data['pengajuan']->id_tipeversi);
+
+		$this->load->library('excel');
 		
+		$iterasi = 0;		
+		foreach ($data['standar'] as $item) {
+
+		if ($iterasi > 0) {
+			$this->excel->createSheet();
+    		$sheet = $this->excel->setActiveSheetIndex($iterasi);
+		} else {
+			$this->excel->setActiveSheetIndex($iterasi);
+		}
+		
+		$iterasi++;
+		$this->excel->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
+		
+		$this->excel->getActiveSheet()->setTitle('Standar ' . $item->nomor);
+		
+		foreach(range('A','C') as $columnID) {
+		    $this->excel->getActiveSheet()->getColumnDimension($columnID)
+		        ->setAutoSize(true);
+		}
+
+		$this->excel->getActiveSheet()->setCellValue('A1', 'DATA LIMBAH B3 YANG KELUAR DARI TPS');
+		$this->excel->getActiveSheet()->getStyle('A1')->getFont()->setSize(20);
+		$this->excel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+		$this->excel->getActiveSheet()->mergeCells('A1:G1');
+		$this->excel->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+		$this->excel->getActiveSheet()->getStyle('A3:G3')->getFont()->setBold(true); 
+		$this->excel->getActiveSheet()->setCellValue('A3', 'STANDAR');
+		$this->excel->getActiveSheet()->setCellValue('B3', 'SUBSTANDAR');
+		$this->excel->getActiveSheet()->setCellValue('C3', 'BUTIR');
+		$this->excel->getActiveSheet()->setCellValue('D3', 'BUTIR PENILAIAN');
+		$this->excel->getActiveSheet()->setCellValue('E3', 'BOBOT');
+		$this->excel->getActiveSheet()->setCellValue('F3', 'NILAI');
+		$this->excel->getActiveSheet()->setCellValue('G3', 'LIST DOKUMEN');
+
+		$i = 1;
+		$a = 4;
+		$styleArray = array(
+		      'borders' => array(
+		          'allborders' => array(
+		              'style' => PHPExcel_Style_Border::BORDER_THIN
+		          )
+		      )
+		  );
+		  $styleArrayBold = array(
+		      'borders' => array(
+		          'allborders' => array(
+		              'style' => PHPExcel_Style_Border::BORDER_MEDIUM
+		          )
+		      )
+		  );
+		
+		$id_standar = 0; $id_substandar = 0; $id_butir = 0; $id_listdokumen = 0; 
+	      foreach ($this->m_penilaian->ambil_detail($item->id) as $item2) {
+	          if ($id_standar == $item2->id_standar) {
+	            $standar = null;
+	          } else {
+	            $standar = $item2->nomor_standar . ' ' . $item2->nama_standar;
+	          }
+	          
+	          if ($id_substandar == $item2->id_substandar) {
+	            $substandar = null;
+	          } else {
+	            $substandar = $item2->nomor_substandar . ' ' . $item2->nama_substandar;
+	          }
+	          
+	          $jml_lido = count($this->db->get_where('butirpenilaian', array('butir_id' => $item2->id_butir))->result());
+	          if ($id_butir == $item2->id_butir) {
+	            $butir = null;
+	            $lido = null;
+	          } else {
+	            $butir = $item2->nomor_butir . ' ' . $item2->nama_butir;
+	            $isi_lido = null;
+	            foreach ($this->db->get_where('listdokumen', array('butir_id' => $item2->id_butir))->result() as $item3) {
+	              $temp_lido = $this->db->get_where('dokumen', array('listdokumen_id' => $item3->id))->result();
+	              if ($temp_lido != null) {
+	                foreach ($temp_lido as $item4) {
+	                  $isi_lido .= '<p><a href="'.base_url($item4->url).'">' . $item4->nama_file . '</a></p>'; 
+	                }
+	              }
+	            }
+	            // $lido = "<td rowspan='".$jml_lido."'>".$isi_lido."</td>";
+	            $lido2 = $isi_lido;
+	          }
+	        
+	          $nilai = null;
+	          $nilai_tmp = $this->db->get_where('detilpenilaian', array('butirpenilaian_id' => $item2->id_butir_penilaian, 'penilaian_id' => $data['penilaian_id']->id))->row();
+	          if ($nilai_tmp != null) {
+	            $nilai = $nilai_tmp->nilai;
+	          }
+
+			$this->excel->getActiveSheet()->setCellValue('A' . $a, $standar);
+			$this->excel->getActiveSheet()->setCellValue('B' . $a, $substandar);
+			$this->excel->getActiveSheet()->setCellValue('C' . $a, $butir);
+			$this->excel->getActiveSheet()->setCellValue('D' . $a, $item2->nomor_butirpenilaian . ' ' . $item2->deskripsi);
+			$this->excel->getActiveSheet()->setCellValue('E' . $a, $item2->bobot);
+			$this->excel->getActiveSheet()->setCellValue('F' . $a, $nilai);
+			$this->excel->getActiveSheet()->setCellValue('G' . $a, $lido2);
+
+	          $id_standar = $item2->id_standar; $id_substandar = $item2->id_substandar; $id_butir = $item2->id_butir;
+	          $i++; $a++;
+	      }
+		
+		}
+		$filename='test.xlsx'; 
+		header('Content-Type: application/vnd.ms-excel'); 
+		header('Content-Disposition: attachment;filename="'.$filename.'"'); 
+		header('Cache-Control: max-age=0'); 
+		
+		$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel2007');  
+		
+		$objWriter->save('php://output');
 	}
 }
