e0781439bf15f23c36660eccacee51e1ce3e0d89
agungdh <agungdh@live.com>
2018-04-16 11:35:52 +0700

    update

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -1,8 +1,15 @@
 <li class="header">MENU UTAMA</li>
 
 <li>
+	<a href="<?php echo base_url(); ?>">
+		<i class="fa fa-dashboard text-aqua"></i>
+		<span>Dashboard</span>
+	</a>
+</li>
+
+<li>
 	<a href="<?php echo base_url("fakultas"); ?>">
-		<i class="fa fa-users text-aqua"></i>
+		<i class="fa fa-building text-aqua"></i>
 		<span>Data Fakultas</span>
 	</a>
 </li>
@@ -16,21 +23,21 @@
 
 <li>
 	<a href="<?php echo base_url("versi"); ?>">
-		<i class="fa fa-users text-aqua"></i>
+		<i class="fa fa-tags text-aqua"></i>
 		<span>Data Borang</span>
 	</a>
 </li>
 
 <li>
 	<a href="<?php echo base_url("pengajuan"); ?>">
-		<i class="fa fa-users text-aqua"></i>
+		<i class="fa fa-briefcase text-aqua"></i>
 		<span>Data Pengajuan</span>
 	</a>
 </li>
 
 <li>
 	<a href="<?php echo base_url("type_dokumen"); ?>">
-		<i class="fa fa-users text-aqua"></i>
+		<i class="fa fa-file text-aqua"></i>
 		<span>Tipe List Dokumen</span>
 	</a>
 </li>
